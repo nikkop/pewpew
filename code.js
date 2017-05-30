24d74e7e49727a565d0c5c8932cb9e129702b428
Nikola Pantic <nikola.pantic@cybercom.com>
2017-05-30 15:23:56 +0200

    Tanks aiming at mouse

--- code.js
+++ code.js
@@ -1,3 +1,34 @@
+const Vec2 = (function(){
+   function sub(v1, v2) {
+      return [
+         v1[0] - v2[0],
+         v1[1] - v2[1]
+      ];
+   }
+
+   function dot(v1, v2) {
+      return v1[0] * v2[0] + v1[1] * v2[1];
+   }
+
+   function normalize(v) {
+      const len = length(v);
+      return [
+         v[0] / len,
+         v[1] / len
+      ];
+   }
+
+   function length(v) {
+      return Math.sqrt((v[0] * v[0]) + (v[1] * v[1]));
+   }
+
+   return {
+      sub,
+      dot,
+      normalize,
+   };
+})()
+
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
@@ -10,16 +41,47 @@ const entities = [
    },
    {
       position: [250, 150],
-      orientation: Math.PI,
+      orientation: 0,
       color: '#9b59b6',
       size: 20,
+   },
+   {
+      position: [400, 50],
+      orientation: 0,
+      color: '#e74c3c',
+      size: 10
+   },
+   {
+      position: [canvas.width - 100, canvas.height / 2],
+      orientation: 0,
+      color: '#f1c40f',
+      size: 100
    }
 ];
 
+canvas.addEventListener('mousemove', e => {
+   const rect = canvas.getBoundingClientRect();
+   const mousePosition = Vec2.sub(
+      [e.clientX, e.clientY],
+      [rect.left, rect.top]
+   );
+
+   entities.forEach(entity => {
+      const entityToMouse = Vec2.sub(mousePosition, entity.position);
+      const angle = Math.acos(Vec2.dot([1, 0], Vec2.normalize(entityToMouse)));
+      entity.orientation = angle;
+      if(mousePosition[1] < entity.position[1]) {
+         entity.orientation *= -1;
+      }
+   })
+})
+
+render();
+
 function render() {
    // logic
-   entities[0].orientation += 0.01;
-   entities[1].orientation += 0.02;
+   // entities[0].orientation += 0.01;
+   // entities[1].orientation += 0.02;
 
    // render
    ctx.fillStyle = '#ecf0f1';
@@ -43,4 +105,3 @@ function render() {
    requestAnimationFrame(render);
 }
 
-render();
