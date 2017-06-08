5a993b61e3f03262820952954f6b1f8bff79165d
Nikola Pantic <nikola.pantic@cybercom.com>
2017-06-08 15:50:55 +0200

    Implemented various collision detections

--- code.js
+++ code.js
@@ -1,6 +1,10 @@
 const Vec2 = (function(){
    const FORWARD = Object.freeze([1, 0]);
 
+   function fromValues(x, y) {
+      return [x, y];
+   }
+
    function add(v1, v2) {
       return [
          v1[0] + v2[0],
@@ -46,6 +50,7 @@ const Vec2 = (function(){
    }
 
    return {
+      fromValues,
       add,
       sub,
       scale,
@@ -62,7 +67,7 @@ const ctx = canvas.getContext('2d');
 
 const entities = [
    {
-      position: [100, 100],
+      position: [100, canvas.height / 2],
       orientation: 0,
       color: '#27ae60',
       size: 50,
@@ -70,7 +75,7 @@ const entities = [
       shape: 'circle',      
    },
    {
-      position: [250, 150],
+      position: [canvas.width / 2, 50],
       orientation: 0,
       color: '#9b59b6',
       size: 20,
@@ -78,7 +83,7 @@ const entities = [
       shape: 'circle',      
    },
    {
-      position: [400, 50],
+      position: [canvas.width - 50, canvas.height / 2],
       orientation: 0,
       color: '#e74c3c',
       size: 10,
@@ -86,15 +91,15 @@ const entities = [
       shape: 'circle',     
    },
    {
-      position: [canvas.width - 100, canvas.height / 2],
+      position: [canvas.width / 2, canvas.height - 100],
       orientation: 0,
       color: '#f1c40f',
-      size: 100,
+      size: 75,
       tag: 'turret',
       shape: 'circle',
    },
    {
-      position: [200, 400],
+      position: [canvas.width / 2, canvas.height / 2],
       orientation: 0,
       color: 'gray',
       size: 30,
@@ -131,7 +136,7 @@ canvas.addEventListener('mousedown', e => {
             position: Vec2.add(turret.position, offset),
             orientation: turret.orientation,
             color: turret.color,
-            size: 5,
+            size: 20,
             tag: 'projectile',
             shape: 'circle',
          });
@@ -140,13 +145,77 @@ canvas.addEventListener('mousedown', e => {
 
 render();
 
-function collides(entity1, entity2) {
-   const diff = Vec2.sub(entity1.position, entity2.position);
+function circleVsCircle(s1, s2) {
+   const diff = Vec2.sub(s1.center, s2.center);
    const distance = Vec2.length(diff);
-   if(distance < entity1.size + entity2.size) {
+   if(distance < s1.radius + s2.radius) {
       return true;
    }
    return false;
+}
+
+function getRect(entity) {
+   const size = Vec2.fromValues(entity.size, entity.size);
+   return {
+      min: Vec2.sub(entity.position, size),
+      max: Vec2.add(entity.position, size)
+   }  
+}
+
+function getCircle(entity) {
+   return {
+      center: entity.position,
+      radius: entity.size
+   }
+}
+
+
+function rectVsRect(r1, r2) {
+   if(r1.max[0] < r2.min[0]) {
+      return false;
+   }
+   if(r1.max[1] < r2.min[1]) {
+      return false;
+   }
+   if(r1.min[0] > r2.max[0]) {
+      return false;
+   }
+   if(r1.min[1] > r2.max[1]) {
+      return false;
+   }
+   return true;
+}
+
+function rectContainsPoint(r, p) {
+   if(r.max[0] < p[0]) {
+      return false;
+   }
+   if(r.max[1] < p[1]) {
+      return false;
+   }
+   if(r.min[0] > p[0]) {
+      return false;
+   }
+   if(r.min[1] > p[1]) {
+      return false;
+   }
+   return true;
+}
+
+function rectVsCircle(r, s) {
+   if(r.max[0] + s.radius < s.center[0]) {
+      return false;
+   }
+   if(r.max[1] + s.radius < s.center[1]) {
+      return false;
+   }
+   if(r.min[0] - s.radius > s.center[0]) {
+      return false;
+   }
+   if(r.min[1] - s.radius > s.center[1]) {
+      return false;
+   }
+   return true;
 }
 
 function render() {
@@ -154,17 +223,30 @@ function render() {
    entities
       .filter(entity => entity.tag === 'projectile')
       .forEach((projectile, index) => {
-         console.log(index);
          const speed = 3;
          let direction = Vec2.rotate(projectile.orientation);
          direction = Vec2.scale(direction, speed);
          projectile.position = Vec2.add(projectile.position, direction);
          
-         const enemy = entities.find(entity => entity.tag === 'enemy');
-         if(collides(projectile, enemy)) {
-            entities.splice(entities.indexOf(projectile), 1);
-            enemy.color = projectile.color;           
-         }
+         entities
+            .filter(entity => entity.tag.match(/projectile|enemy/))
+            .filter(entity => entity !== projectile)
+            .filter(entity => entity.color !== projectile.color)
+            .forEach(entity => {
+               if(entity.shape === 'circle') {
+                  if(circleVsCircle(getCircle(entity), getCircle(projectile))) {
+                     entities.splice(entities.indexOf(projectile), 1);
+                     entity.color = projectile.color;  
+                     entity.size += projectile.size * 0.1;
+                  }
+               }
+               else {
+                  if(rectVsCircle(getRect(entity), getCircle(projectile))) {
+                     entities.splice(entities.indexOf(projectile), 1);
+                     entity.color = projectile.color;           
+                  }
+               }
+            })
       });
 
    // render
